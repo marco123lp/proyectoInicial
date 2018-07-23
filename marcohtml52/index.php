cb3b93ab4f297391fe8ab1b3c340a5894d2ac779
marco <marcoadg70@hotmail.com>
2018-07-23 06:19:13 -0400

    subida

---
+++ marcohtml52/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Marquito</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta name="description" content="pagina de prueba aprende con tu docente gordito">
+	<meta name="KEYWORDS" content="pagina prueba, ejemplo, aplicacion de prueba">	
+	<link rel="shortcut icon" type="image/x-icon" href="img/loguito.jpg">
+	<link rel="canonical" href="https://www.marquito.com/">
+</head>
+<body>
+	<section>
+		<header>
+			<nav>
+				<ul>
+					<li><a href="">Inicio</a></li>
+					<li><a href="">Quines Somos</a></li>
+					<li><a href="">Descargas</a></li>
+					<li><a href="">Comentarios</a></li>
+					<li><a href="">Sugerencias</a></li>
+				</ul>
+			</nav>
+		</header>
+		<article>
+			<figure>
+				<h2>Este es un titulo</h2>
+				<img src="img/3.jpg" alt="">
+				<figcaption>imagen descargadad de: <a href="https://www.slideshare.net/jorandwen/html-y-html/2">www.slideshare.net/jorandwen</a></figcaption>
+				<cite>|referencia a la imagen usada</cite>
+				<time>hora: 01:03am</time>
+				<p>Lorem <strong>Este texto es realmente importante</strong> ipsum dolor sit amet, <em>este es un texto de relevancia</em> consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				<blockquote>Este texto es resaltado como una cita de importancia</blockquote>
+				<mark>resaltare un texto</mark>
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</figure>
+		</article>
+		<hr>
+		<aside>
+			<h3>Este es un aside</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</aside>
+		<hr>
+		<footer>
+			<center><small><strong>	<br>&;copyMarco Anotnio Dorado Gomez <br>La Paz - Bolivia <br>2018</strong></small></center>
+		</footer>
+	</section>
+</body>
+</html>
